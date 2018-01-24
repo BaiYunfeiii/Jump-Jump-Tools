053428252520f72234c256a21ae4696fc91c0117
Bai Yunfei <baiyunfeiii@foxmail.com>
2018-01-24 21:49:45 +0800

    修改

--- Parser.py
+++ Parser.py
@@ -27,58 +27,43 @@ class Parser:
         return a == b
     
     def getStartAndEndPoints(filename, debug=False):
-        with open(filename,'rb') as f:
-            img = Image.open(f)
-#            bg = img.getpixel((int(img.height/4),int(img.height*2/3))) #背景颜色
-            bg = img.getpixel((0,Parser.score_bottom))
+        
+        step = 3
+        
+        img = Image.open(filename)
+        bg = img.getpixel((0,0)) #背景颜色
+    
+        box_color = None #目标棋盘的颜色   
+        
+        target = { #目标棋盘的坐标
+            'top':[-1,-1],
+            'left':[-1,-1],
+            'bottom':[-1,-1],
+            'right':[-1,-1]    
+        }
+        
+        chess_point = [-1,-1]
+        
+        left_fix = False
+        right_fix = False
+        bottom_fix = False
+    
+        for y in range(Parser.score_bottom, int(img.height*2/3),2):
             
-#            chess_point,target = Parser.praseTarget(bg,img,debug=debug)
+            border = [-1,-1] # (start,end)
             
-            box_color = None #目标棋盘的颜色   
+            chess_border = [-1,-1]
             
-            target = { #目标棋盘的坐标
-                'top':[],
-                'left':[],
-                'bottom':[],
-                'right':[]    
-            }
-            
-            chess_point = [-1,-1]
-            
-            left_fix = False
-            right_fix = False
-
-            for y in range(int(img.height/4), int(img.height*2/3),1):
+            #扫描每一列
+            for x in range(0, img.width, step):
+                pixel = img.getpixel((x,y))
                 
-                border = [-1,-1] # (start,end)
+                bg_sim = Parser.distance(bg,pixel,0.005)
+                if(bg_sim):
+                    bg = pixel
+                    continue
                 
-                chess_border = [-1,-1]
-
-                skip = 0           
-                
-                #扫描每一列
-                for x in range(0,img.width,5):
-                    pixel = img.getpixel((x,y))
-
-                    bg_sim = Parser.distance(bg, pixel)                    
-                    
-#                    if(bg_sim):
-#                        bg = pixel
-                        
-                    if(box_color == None and (not bg_sim) and (not Parser.distance(Parser.chess_color,pixel,0.01))):
-                        if(skip > 0):
-                            skip = skip - 1
-                            continue
-                        box_color = pixel
-                        
-                    #目标盒子的坐标
-                    if(box_color == pixel):
-                        if(border[0] == -1):
-                            border[0] = x
-                            border[1] = x
-                        else:
-                            border[1] = x
-                    
+                if(Parser.distance(Parser.chess_color,pixel,0.1)):
                     #找棋子的坐标
                     if(Parser.distance(Parser.chess_color, pixel,0.005)):
                         if(chess_border[0] == -1):
@@ -86,41 +71,74 @@ class Parser:
                             chess_border[1] = x
                         else:
                             chess_border[1] = x
-                
-                if(chess_border[0] != -1):
-                    chess_point = [(chess_border[0] + chess_border[1])/2, y]           
-                                    
-                if(border[0] == -1):
                     continue
                 
-                if(target['top'] == []):
-                    target['top'] = [(border[0]+border[1])/2,y]
-                    continue
-                
-                if(target['left'] == []):
-                    target['left'] = [border[0],y]
-                    target['right'] = [border[1],y]
-                    continue
-                if(target['left'][0] >= border[0] and (not left_fix)):
-                    target['left'] = [border[0],y]
-                else:
-                    left_fix = True
-                if(target['right'][0] <= border[1] and (not right_fix)):
-                    target['right'] = [border[1],y]
-                else:
-                    right_fix = True
+                #和背景、棋子的颜色不同
+                if(box_color == None):
+                    box_color = pixel
+                    print(box_color)
                     
-                target['bottom'] = [(border[0]+border[1])/2,y]
+                #目标盒子的坐标
+                if(Parser.distance(box_color,pixel,0.001)):
+                    if(border[0] == -1):
+                        border[0] = x
+                        border[1] = x
+                    else:
+                        border[1] = x
                 
-            try:
-                center = Parser.revise(target)
-                return chess_point,center,target
-            except:
-                print('err:' + str(target))
-                chess_point,target = Parser.praseTarget(bg,img,Parser.distance,p=0.1,debug=debug)
-                center = Parser.revise(target)
-#               Parser.draw(chess_point, center, filename, target)
-                return chess_point,center,target
+            
+            if(chess_border[0] != -1):
+                chess_point = [(chess_border[0] + chess_border[1])/2, y]    
+                continue
+                                
+            if(border[0] == -1):
+                continue
+            
+            if(target['top'] == [-1,-1]):
+                target['top'] = [(border[0]+border[1])/2,y]
+                continue
+            
+            if(target['left'] == [-1,-1]):
+                target['left'] = [border[0],y]
+                target['right'] = [border[1],y]
+                continue
+            if(target['left'][0] > border[0] and (not left_fix)):
+                target['left'] = [border[0],y]
+            else:
+                left_fix = True
+            if(target['right'][0] < border[1] and (not right_fix)):
+                target['right'] = [border[1],y]
+            else:
+                right_fix = True
+            
+            if(not bottom_fix):
+                target['bottom'] = [(border[0]+border[1])/2,y]
+            
+            if(target['bottom'] != [-1,-1] and (not bottom_fix) and border[0] == -1):
+                bottom_fix = True
+                
+        print(target)
+        #差错检验
+        try:
+            center = [-1,-1]
+            if(target['left'][1] == -1 or target['left'][0] == -1):
+                if(target['right'][0] == -1 or target['right'][1] == -1):
+                    target['left'] = [target['top'][0], target['top'][1] + 30]
+                    target['right'] = target['left']
+                else:
+                    target['left'] = target['right']
+    
+            if(target['bottom'][0] < target['left'][0] or target['bottom'][0] > target['right'][0]):
+                target['bottom'][0] = target['top'][0]
+                
+            center[0] = (target['top'][0] + target['bottom'][0])/2
+            center[1] = (target['left'][1] + target['right'][1])/2
+        except:
+            print('center calculate err: ',target)
+        finally:
+            Parser.draw(chess_point,center,filename,target)
+        
+        return chess_point, center, target
     
     def revise(target):
         left = True
@@ -143,97 +161,13 @@ class Parser:
         center[0] = (target['left'][0] + target['right'][0])/2
         center[1] = (target['top'][1] + target['bottom'][1])/2
         return center
-    
-    def praseTarget(bg,img, sim=eq,p=0.01, debug = False):
-        box_color = None #目标棋盘的颜色   
-        
-        target = { #目标棋盘的坐标
-            'top':[],
-            'left':[],
-            'bottom':[],
-            'right':[]    
-        }
-        
-        chess_point = [-1,-1]
-        
-        left_fix = False
-        right_fix = False
-
-        for y in range(int(img.height/4), int(img.height*2/3),1):
-            
-            border = [-1,-1] # (start,end)
-            
-            chess_border = [-1,-1]
-
-            skip = 0               
-            
-            #扫描每一列
-            for x in range(0,img.width,5):
-                pixel = img.getpixel((x,y))
-
-                bg_sim = Parser.distance(bg, pixel,0.01)                    
-                
-                if(bg_sim):
-                    bg = pixel
-                    
-                if(box_color == None and (not bg_sim) and (not Parser.distance(Parser.chess_color,pixel))):
-                    if(skip > 0):
-                        skip = skip - 1
-                        continue
-                    box_color = pixel
-                else:
-                    continue
-                    
-                #目标盒子的坐标
-                if(sim(box_color, pixel, p)):
-                    print(box_color,pixel)
-                    if(border[0] == -1):
-                        border[0] = x
-                        border[1] = x
-                    else:
-                        border[1] = x
-                
-                #找棋子的坐标
-                if(Parser.distance(Parser.chess_color, pixel,0.005)):
-                    if(chess_border[0] == -1):
-                        chess_border[0] = x
-                        chess_border[1] = x
-                    else:
-                        chess_border[1] = x
-            
-            if(chess_border[0] != -1):
-                chess_point = [(chess_border[0] + chess_border[1])/2, y]           
-                                
-            if(border[0] == -1):
-                continue
-            
-            if(target['top'] == []):
-                target['top'] = [(border[0]+border[1])/2,y]
-                continue
-            
-            if(target['left'] == []):
-                target['left'] = [border[0],y]
-                target['right'] = [border[1],y]
-                continue
-            if(target['left'][0] >= border[0] and (not left_fix)):
-                target['left'] = [border[0],y]
-            else:
-                left_fix = True
-            if(target['right'][0] <= border[1] and (not right_fix)):
-                target['right'] = [border[1],y]
-            else:
-                right_fix = True
-                
-            target['bottom'] = [(border[0]+border[1])/2,y]
-        if(debug == True):
-            print(target)
-        return chess_point,target
             
     def draw(chess_point, center, filename, target=None):           
         lena = mpimg.imread(filename) #读取和代码处于同一目录下的lena.png
-        plt.figure(num=1,figsize=(30,18))
+        plt.figure(num=1,figsize=(15,9))
         plt.imshow(lena) # 显示图片
-        plt.scatter(center[0],center[1],color='b')
+        if(center != None):
+            plt.scatter(center[0],center[1],color='b')
         plt.scatter(chess_point[0],chess_point[1],color='g')
         if(target != None):
             plt.scatter(target['top'][0],target['top'][1], color='r', marker='o')
@@ -242,3 +176,7 @@ class Parser:
             plt.scatter(target['bottom'][0],target['bottom'][1], color='r', marker='.')
         plt.show()
         
+if __name__ == '__main__':
+    filename = 'screen/p_0.png'
+    chess_point, center, target = Parser.getStartAndEndPoints(filename)
+    Parser.draw(chess_point,center,filename,target)
